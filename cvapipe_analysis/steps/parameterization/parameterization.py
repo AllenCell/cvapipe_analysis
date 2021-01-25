5f93a3a7826c48c39e72e97538103c0eb025ac14
Matheus Viana <matheus.viana@alleninstitute.org>
2021-01-25 11:57:45 -0800

    Merge metadata and features

--- cvapipe_analysis/steps/parameterization/parameterization.py
+++ cvapipe_analysis/steps/parameterization/parameterization.py
@@ -31,7 +31,29 @@ class Parameterization(Step):
         debug=False,
         **kwargs):
 
-        self.manifest = df_features
+        # For parameterization we need to load the single cell
+        # metadata dataframe and the single cell feature dataframe
+
+        # Load manifest from load_data step
+        df = pd.read_csv(
+            self.project_local_staging_dir/'loaddata/manifest.csv',
+            index_col = 'CellId'
+        )
+        
+        # Keep only the columns that will be used from now on
+        columns_to_keep = ['crop_raw', 'crop_seg', 'name_dict']
+        df = df[columns_to_keep]
+        
+        # Load manifest from feature calculation step
+        df_features = pd.read_csv(
+            self.project_local_staging_dir/'computefeatures/manifest.csv',
+            index_col = 'CellId'
+        )
+                
+        # Merge the two dataframes
+        df = df.join(df_features, how='inner')
+        
+        self.manifest = df
         manifest_save_path = self.step_local_staging_dir / "manifest.csv"
         self.manifest.to_csv(manifest_save_path)
             
