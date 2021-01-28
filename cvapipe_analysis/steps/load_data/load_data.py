41b8b704d5e0b463d6ff14639bc71b6b753d4fdb
Matheus Viana <matheus.viana@alleninstitute.org>
2021-01-28 13:52:27 -0800

    Add `test` flag to allow for downloading a small sample of the dataset

--- cvapipe_analysis/steps/load_data/load_data.py
+++ cvapipe_analysis/steps/load_data/load_data.py
@@ -17,34 +17,40 @@ log = logging.getLogger(__name__)
 
 
 def download_quilt_data(
-    package="aics/hipsc_single_cell_image_dataset",
+    package_name="aics/hipsc_single_cell_image_dataset",
     registry="s3://allencell",
     data_save_loc="quilt_data",
     ignore_warnings=True,
+    test=False,
 ):
     """Download a quilt dataset and supress nfs file attribe warnings by default"""
-    dataset_manifest = quilt3.Package.browse(package, registry)
+    pkg = quilt3.Package.browse(package_name, registry)
+    
+    meta_df = pkg["metadata.csv"]()
+    
+    if test:
+        print('>> Downloading test dataset...')
+        meta_df = meta_df.sample(n=300, random_state=666)
 
-    meta_df = dataset_manifest["metadata.csv"]()
-
-    if ignore_warnings:
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore", UserWarning)
-
-            # download single cell raw images (cell membrane dye, dna dye, structure)
-            dataset_manifest["crop_raw"].fetch(data_save_loc / Path("crop_raw"))
-
-            # download single cell segmentation images (cell seg, nucleus seg,
-            # and structure seg)
-            dataset_manifest["crop_seg"].fetch(data_save_loc / Path("crop_seg"))
-
-    else:
-        # download single cell raw images (cell membrane dye, dna dye, structure)
-        dataset_manifest["crop_raw"].fetch(data_save_loc / Path("crop_raw"))
-
-        # download single cell segmentation images (cell seg, nucleus seg,
-        #  and structure seg)
-        dataset_manifest["crop_seg"].fetch(data_save_loc / Path("crop_seg"))
+    # Creating directories
+    path_to_raw_folder = (data_save_loc / Path("crop_raw"))
+    path_to_raw_folder.mkdir(parents=True, exist_ok=True)
+    path_to_seg_folder = (data_save_loc / Path("crop_seg"))
+    path_to_seg_folder.mkdir(parents=True, exist_ok=True)
+        
+    for i, row in meta_df.iterrows():
+        if ignore_warnings:
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore", UserWarning)
+                # download single cell raw and segmentation images
+                # (cell membrane dye, dna dye, structure)
+                pkg[row["crop_raw"]].fetch(path_to_raw_folder)
+                pkg[row["crop_seg"]].fetch(path_to_seg_folder)
+        else:
+            # download single cell raw and segmentation images
+            # (cell membrane dye, dna dye, structure)
+            pkg[row["crop_raw"]].fetch(path_to_raw_folder)
+            pkg[row["crop_seg"]].fetch(path_to_seg_folder)
 
     return meta_df
 
@@ -64,9 +70,10 @@ class LoadData(Step):
     @log_run_params
     def run(
         self,
-        package="aics/hipsc_single_cell_image_dataset",
+        package_name="aics/hipsc_single_cell_image_dataset",
         registry="s3://allencell",
         data_save_loc="quilt_data",
+        test=False,
         **kwargs
     ):
         """
@@ -83,6 +90,8 @@ class LoadData(Step):
             A debug flag for the developer to use to manipulate how much data runs,
             how it is processed, etc.
             Default: False (Do not debug)
+        test: bool
+            Download only a small test dataset of 300 cells chosen at random.
 
         Parameters
         ----------
@@ -93,10 +102,11 @@ class LoadData(Step):
             A pickable object or value that is the result of any processing you do.
         """
         dataset = download_quilt_data(
-            package=package,
+            package_name=package_name,
             registry=registry,
             data_save_loc=self.step_local_staging_dir,
             ignore_warnings=True,
+            test=test
         )
         
         self.manifest = dataset
