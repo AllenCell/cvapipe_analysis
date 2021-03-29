7b48914deca1716dde4a5017501d24e5a921d0fd
Matheus Viana <vianamp@gmail.com>
2021-03-29 14:40:21 -0700

    config as context manager

--- cvapipe_analysis/steps/concordance/concordance.py
+++ cvapipe_analysis/steps/concordance/concordance.py
@@ -36,58 +36,59 @@ class Concordance(Step):
         **kwargs
     ):
 
-        # Load configuration file
-        config = general.load_config_file()
-        
-        # Make necessary folders
-        for folder in ['values', 'plots']:
-            save_dir = self.step_local_staging_dir / folder
-            save_dir.mkdir(parents=True, exist_ok=True)
+        with general.configuration(self.step_local_staging_dir) as config:
+    
+            # Make necessary folders
+            for folder in ['values', 'plots']:
+                save_dir = self.step_local_staging_dir / folder
+                save_dir.mkdir(parents=True, exist_ok=True)
 
-        # Create all combinations of parameters
-        space = shapespace.ShapeSpaceBasic(config)
-        df_agg = space.iter_param_values_as_dataframe(
-            config, [
-                ('aggtype', ['avg']),
-                ('intensity', space.iter_intensities),
-                ('structure_name1', space.iter_structures),
-                ('structure_name2', space.iter_structures),
-                ('shapemode', space.iter_shapemodes),
-                ('bin', space.iter_bins)
-            ], [('structure_name1', 'structure_name2')]
-        )
+            # Create all combinations of parameters
+            space = shapespace.ShapeSpaceBasic(config)
+            df_agg = space.iter_param_values_as_dataframe(
+                config, [
+                    ('aggtype', ['avg']),
+                    ('intensity', space.iter_intensities),
+                    ('structure_name1', space.iter_structures),
+                    ('structure_name2', space.iter_structures),
+                    ('shapemode', space.iter_shapemodes),
+                    ('bin', space.iter_bins)
+                ], [('structure_name1', 'structure_name2')]
+            )
 
-        if distribute:
-            
-            nworkers = config['resources']['nworkers']            
-            distributor = cluster.ConcordanceDistributor(df_agg, nworkers)
-            distributor.distribute(config, log)
+            if distribute:
 
-            log.info(f"Multiple jobs have been launched. Please come back when the calculation is complete.")
-            return None
-                        
-        calculator = ConcordanceCalculator(config)            
-        with concurrent.futures.ProcessPoolExecutor(cluster.get_ncores()) as executor:
-            paths = list(executor.map(calculator.execute, [row for _,row in df_agg.iterrows()]))
-        df_agg['PathToConcordanceFile'] = paths
-        
-        log.info(f"Loading results...")
-        
-        df_results = calculator.load_results_in_single_dataframe()
+                nworkers = config['resources']['nworkers']            
+                distributor = cluster.ConcordanceDistributor(df_agg, nworkers)
+                distributor.distribute(config, log)
 
-        log.info(f"Generating plots...")
-        
-        space  = shapespace.ShapeSpaceBasic(config)
-        pmaker = plotting.ConcordancePlotMaker(config)
-        pmaker.set_dataframe(df_results)
-        for intensity in tqdm(space.iter_intensities(config)):
-            for shapemode in space.iter_shapemodes(config):
-                pmaker.filter_dataframe({'intensity':intensity, 'shapemode':shapemode, 'bin':[5]})
-                pmaker.execute(display=False)
-                pmaker.filter_dataframe({'intensity':intensity, 'shapemode':shapemode, 'bin':[1,9]})
-                pmaker.execute(display=False)
-        
-        self.manifest = df_agg
-        manifest_path = self.step_local_staging_dir / 'manifest.csv'
-        self.manifest.to_csv(manifest_path)
+                log.info(f"Multiple jobs have been launched. Please come back when the calculation is complete.")
+                return None
+
+            calculator = ConcordanceCalculator(config)            
+            with concurrent.futures.ProcessPoolExecutor(cluster.get_ncores()) as executor:
+                paths = list(executor.map(calculator.execute, [row for _,row in df_agg.iterrows()]))
+            df_agg['PathToConcordanceFile'] = paths
+
+            log.info(f"Loading results...")
+
+            df_results = calculator.load_results_in_single_dataframe()
+
+            log.info(f"Generating plots...")
+
+            space  = shapespace.ShapeSpaceBasic(config)
+            pmaker = plotting.ConcordancePlotMaker(config)
+            pmaker.set_dataframe(df_results)
+            for intensity in tqdm(space.iter_intensities(config)):
+                for shapemode in space.iter_shapemodes(config):
+                    pmaker.filter_dataframe({'intensity':intensity, 'shapemode':shapemode, 'bin':[5]})
+                    pmaker.execute(display=False)
+                    pmaker.filter_dataframe({'intensity':intensity, 'shapemode':shapemode, 'bin':[1,9]})
+                    pmaker.execute(display=False)
+
+            log.info(f"Saving manifest...")
+            self.manifest = df_agg
+            manifest_path = self.step_local_staging_dir / 'manifest.csv'
+            self.manifest.to_csv(manifest_path)
+
         return manifest_path
--- cvapipe_analysis/steps/stereotypy/stereotypy.py
+++ cvapipe_analysis/steps/stereotypy/stereotypy.py
@@ -34,67 +34,66 @@ class Stereotypy(Step):
         **kwargs
     ):
 
-        # Load configuration file
-        config = general.load_config_file()
-        
-        # Make necessary folders
-        for folder in ['values', 'plots']:
-            save_dir = self.step_local_staging_dir / folder
-            save_dir.mkdir(parents=True, exist_ok=True)
+        with general.configuration(self.step_local_staging_dir) as config:
+    
+            # Make necessary folders
+            for folder in ['values', 'plots']:
+                save_dir = self.step_local_staging_dir / folder
+                save_dir.mkdir(parents=True, exist_ok=True)
 
-        # Create all combinations of parameters to calculate stereotypy
-        space = shapespace.ShapeSpace(config)
-        space.load_shape_space_axes()
-        df_agg = pd.DataFrame()
-        for shapemode in space.iter_shapemodes(config):
-            space.set_active_axis(shapemode, digitize=True)
-            for b in space.iter_bins(config):
-                space.set_active_bin(b)
-                for struct in space.iter_structures(config):
-                    space.set_active_structure(struct)
-                    df_tmp = space.iter_param_values_as_dataframe(
-                        config, [
-                            ('aggtype', ['avg']),
-                            ('intensity', space.iter_intensities),
-                            ('structure_name', [struct]),
-                            ('shapemode', [shapemode]),
-                            ('bin', [b]),
-                            ('CellIds', [space.get_active_cellids()])
-                        ]
-                    )
-                    df_agg = df_agg.append(df_tmp, ignore_index=True)
+            # Create all combinations of parameters to calculate stereotypy
+            space = shapespace.ShapeSpace(config)
+            space.load_shape_space_axes()
+            df_agg = pd.DataFrame()
+            for shapemode in space.iter_shapemodes(config):
+                space.set_active_axis(shapemode, digitize=True)
+                for b in space.iter_bins(config):
+                    space.set_active_bin(b)
+                    for struct in space.iter_structures(config):
+                        space.set_active_structure(struct)
+                        df_tmp = space.iter_param_values_as_dataframe(
+                            config, [
+                                ('aggtype', ['avg']),
+                                ('intensity', space.iter_intensities),
+                                ('structure_name', [struct]),
+                                ('shapemode', [shapemode]),
+                                ('bin', [b]),
+                                ('CellIds', [space.get_active_cellids()])
+                            ]
+                        )
+                        df_agg = df_agg.append(df_tmp, ignore_index=True)
 
-        if distribute:
-            
-            nworkers = config['resources']['nworkers']            
-            distributor = cluster.StereotypyDistributor(df_agg, nworkers)
-            distributor.distribute(config, log)
+            if distribute:
 
-            log.info(f"Multiple jobs have been launched. Please come back when the calculation is complete.")            
-            return None
+                nworkers = config['resources']['nworkers']            
+                distributor = cluster.StereotypyDistributor(df_agg, nworkers)
+                distributor.distribute(config, log)
 
-        calculator = StereotypyCalculator(config)    
-        for index, row in tqdm(df_agg.iterrows(), total=len(df_agg)):
-            '''Concurrent processes inside. Do not use concurrent here.'''
-            df_agg.loc[index,'PathToStereotypyFile'] = calculator.execute(row)
+                log.info(f"Multiple jobs have been launched. Please come back when the calculation is complete.")            
+                return None
 
-        log.info(f"Loading results...")
-        
-        df_results = calculator.load_results_in_single_dataframe()
+            calculator = StereotypyCalculator(config)    
+            for index, row in tqdm(df_agg.iterrows(), total=len(df_agg)):
+                '''Concurrent processes inside. Do not use concurrent here.'''
+                df_agg.loc[index,'PathToStereotypyFile'] = calculator.execute(row)
 
-        log.info(f"Generating plots...")
-        
-        space  = shapespace.ShapeSpaceBasic(config)
-        for intensity in space.iter_intensities(config):
-            pmaker = plotting.StereotypyPlotMaker(config)
-            pmaker.set_dataframe(df_results)
-            pmaker.filter_dataframe({'intensity': intensity, 'shapemode': 'DNA_MEM_PC1', 'bin': 5})
-            pmaker.execute(display=False)
-            pmaker.set_max_number_of_pairs(300)
-            pmaker.execute(display=False, prefix='N300')
-            
-        self.manifest = df_agg
-        manifest_path = self.step_local_staging_dir / 'manifest.csv'
-        self.manifest.to_csv(manifest_path)
+            log.info(f"Loading results...")
+
+            df_results = calculator.load_results_in_single_dataframe()
+
+            log.info(f"Generating plots...")
+
+            space  = shapespace.ShapeSpaceBasic(config)
+            for intensity in space.iter_intensities(config):
+                pmaker = plotting.StereotypyPlotMaker(config)
+                pmaker.set_dataframe(df_results)
+                pmaker.filter_dataframe({'intensity': intensity, 'shapemode': 'DNA_MEM_PC1', 'bin': 5})
+                pmaker.execute(display=False)
+                pmaker.set_max_number_of_pairs(300)
+                pmaker.execute(display=False, prefix='N300')
+
+            self.manifest = df_agg
+            manifest_path = self.step_local_staging_dir / 'manifest.csv'
+            self.manifest.to_csv(manifest_path)
 
         return manifest_path
