57753aeb1d08380d2074e8efff986702a42e0c50
Matheus Viana <vianamp@gmail.com>
2022-02-08 12:14:36 -0800

    Change for comp with new branch

--- cvapipe_analysis/steps/correlation/correlation_tools.py
+++ cvapipe_analysis/steps/correlation/correlation_tools.py
@@ -105,8 +105,12 @@ if __name__ == "__main__":
     df = df.T
     df.CellIds = df.CellIds.astype(object)
     for index, row in df.iterrows():
-        df.at[index,"CellIds"] = eval(row.CellIds)
+        df.at[index,"CellIds"] = eval(row.CellIds)[:600]
     calculator = CorrelationCalculator(control)
     for _, row in df.iterrows():
         '''Concurrent processes inside. Do not use concurrent here.'''
-        calculator.execute(row)
+        calculator.set_row(row)
+        calculator.workflow()
+        print(f"<REPS> = {calculator.reps.mean():.10f}")
+        print(f"<CORR> = {calculator.corrs.mean():.10f}") 
+
