0bee2c4bce2d86e813e6930156a81b58b84715c5
Matheus Viana <vianamp@gmail.com>
2022-03-14 15:39:58 -0700

    No NsSphere for matched ds

--- cvapipe_analysis/steps/stereotypy/stereotypy.py
+++ cvapipe_analysis/steps/stereotypy/stereotypy.py
@@ -44,22 +44,21 @@ class Stereotypy(Step):
             space.execute(df)
             variables = control.get_variables_values_for_aggregation()
             df_agg = space.get_aggregated_df(variables, include_cellIds=False)
-            variables.update({"shape_mode": ["NdSphere"], "mpId": [control.get_center_map_point_index()]})
-            df_sphere = space.get_aggregated_df(variables, include_cellIds=False)
-            df_agg = df_agg.append(df_sphere, ignore_index=True)
+            '''Do not aggregate cells inside nD sphere when working with
+            datasets with a single map point, which is the case of matched datasets'''
+            if len(control.get_map_points()) > 1:
+                variables.update({"shape_mode": ["NdSphere"], "mpId": [control.get_center_map_point_index()]})
+                df_sphere = space.get_aggregated_df(variables, include_cellIds=False)
+                df_agg = df_agg.append(df_sphere, ignore_index=True)
             df_agg =  df_agg.drop(columns=["structure"]).drop_duplicates().reset_index(drop=True)
 
             log.info(f"Generating plots...")
 
             for index, row in tqdm(df_agg.iterrows(), total=len(df_agg)):
-                try:
-                    pmaker = plotting.StereotypyPlotMaker(control)
-                    pmaker.set_heatmap_min_max_values(-0.2, 0.2)
-                    pmaker.set_dataframe(df)
-                    pmaker.set_row(row)
-                    if row.mpId == 1:
-                        pmaker.set_extra_values({"mpId": df_agg.mpId.unique().tolist()})
-                    pmaker.execute(display=False)
-                except: pass
-
-
+                pmaker = plotting.StereotypyPlotMaker(control)
+                pmaker.set_heatmap_min_max_values(-0.2, 0.2)
+                pmaker.set_dataframe(df)
+                pmaker.set_row(row)
+                if row.mpId == 1:
+                    pmaker.set_extra_values({"mpId": df_agg.mpId.unique().tolist()})
+                pmaker.execute(display=False)
