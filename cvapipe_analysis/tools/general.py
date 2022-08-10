4d7171919d08ad1c8bc15834b91cace881693241
Matheus Viana <vianamp@gmail.com>
2022-08-10 13:46:00 -0700

    Adjust load nad save config

--- cvapipe_analysis/tools/general.py
+++ cvapipe_analysis/tools/general.py
@@ -13,9 +13,9 @@ def load_config_file(path: Path="./", fname="config.yaml"):
     return config
 
 
-def save_config_file(path_to_folder):
+def save_config_file(path_to_folder, filename="parameters.yaml"):
     path_to_folder = Path(path_to_folder)
-    shutil.copyfile("./config.yaml", path_to_folder/"parameters.yaml")
+    shutil.copyfile("./config.yaml", path_to_folder/filename)
     return
 
 
@@ -29,10 +29,9 @@ def create_workflow_file_from_config():
 def get_date_time():
     return datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
-
 @contextmanager
 def configuration(path_to_folder=None):
-    config = load_config_file()
+    config = load_config_file(path=path_to_folder)
     control = controller.Controller(config)
     try:
         control.log({"start": get_date_time()})
