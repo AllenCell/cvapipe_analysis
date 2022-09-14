5880284b6577fc7aed600be0715d4990d03a7f86
Matheus Viana <vianamp@gmail.com>
2022-09-13 18:52:37 -0700

    Distributed features decoupled

--- cvapipe_analysis/steps/compute_features/compute_features.py
+++ cvapipe_analysis/steps/compute_features/compute_features.py
@@ -48,8 +48,8 @@ class ComputeFeatures(Step):
             calculator = FeatureCalculator(control)
             if verbose:
                 calculator.set_verbose_mode_on()
-            for _, row in df.iterrows():
-                calculator.execute(row)
+            with concurrent.futures.ProcessPoolExecutor(control.get_ncores()) as executor:
+                executor.map(calculator.execute, [row for _,row in df.iterrows()])
 
             log.info(f"Loading results...")
             df_results = calculator.load_results_in_single_dataframe()
