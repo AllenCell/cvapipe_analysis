c82052d5b216e7089aadd50e6eee1246096be83d
Ritvik Vasan <ritvik.vasan@alleninstitute.org>
2022-12-14 11:52:44 -0800

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -113,6 +113,6 @@ setup(
     url="https://github.com/AllenCellModeling/cvapipe_analysis",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="0.1.2",
+    version="0.1.3",
     zip_safe=False,
 )
